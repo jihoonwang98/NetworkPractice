e6bfe5131835a1116be4564510f87af77c7f252c
jihoonwang98 <jihoonwang98@naver.com>
2020-08-13 17:12:27 +0900

    prac

---
+++ conv_addr_win.c
@@ -0,0 +1,34 @@
+#undef UNICODE
+#undef _UNICODE
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main(int argc, char* argv[]) {
+	char* strAddr = "203.211.218.102:9190";
+
+	char strAddrBuf[50];
+	SOCKADDR_IN servAddr;
+	int size;
+
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	size = sizeof(servAddr);
+	WSAStringToAddress(strAddr, AF_INET, NULL, (SOCKADDR*)&servAddr, &size);
+
+	size = sizeof(strAddrBuf);
+	WSAAddressToString((SOCKADDR*)&servAddr, sizeof(servAddr), NULL, strAddrBuf, &size);
+
+	printf("Second conv result: %s\n", strAddrBuf);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
