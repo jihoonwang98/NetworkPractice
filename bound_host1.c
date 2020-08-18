b6768efca25414045c69f7eccf4f86cf6865007d
jihoonwang98 <jihoonwang98@naver.com>
2020-08-18 02:52:40 -0700

    prac

--- bound_host1.c
+++ bound_host1.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    sock= socket(PF_INET, SOCK_DGRAM, 0);
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
     if(sock == -1)
         error_handling("socket() error");
 
